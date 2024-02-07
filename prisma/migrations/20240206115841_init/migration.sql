f4a596b1babc61735d5909bfb9980fa892679ba5
DSDimitrov19 <DSDimitrov19@codingburgas.bg>
2024-02-07 13:52:30 +0200

    fix: remove old stuff

--- prisma/migrations/20240206115841_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Door" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "status" TEXT NOT NULL DEFAULT 'opened',
-    "updatedAt" DATETIME NOT NULL
-);
