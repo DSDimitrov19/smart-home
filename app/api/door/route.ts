57f05c688f3eb68e4c5ea71161978ceb0ebc0327
DSDimitrov19 <DSDimitrov19@codingburgas.bg>
2024-02-13 13:33:47 +0200

    fix: da ima

--- app/api/door/route.ts
+++ app/api/door/route.ts
@@ -9,7 +9,11 @@ interface Door {
 
 export async function GET() {
     // await prisma.user.create({ data: { email: "dimi1004@gmail.com", password: "password123" } })
-    return NextResponse.json(await prisma.door.findMany());
+    return NextResponse.json(await prisma.door.findMany({
+        orderBy: {
+            id: "asc"
+        }
+    }));
 }
 
 export async function POST(request: Request) {
