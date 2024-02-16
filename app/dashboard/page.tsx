6cc199822ae300bb7755514a0543475c4ebcc19a
DSDimitrov19 <DSDimitrov19@codingburgas.bg>
2024-02-16 11:09:25 +0200

    feat: add cards for devices

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import Card from "@/components/card";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const { data: session, status } = useSession({
@@ -10,12 +12,38 @@ export default function Home() {
       redirect("/login");
     },
   });
+  const [ devices, setDevices ] = useState([]);
+
+  const fetchDevices = async () => {
+    try {
+      const response = await fetch('/api/door');
+      const newData = await response.json();
+      setDevices(newData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDevices();
+
+    const interval = setInterval(fetchDevices, 1000);
+
+    return () => clearInterval(interval);
+  }, [])
 
   if (status === "loading") {
     return <></>
   }
 
   return (
-    <button onClick={() => signOut()}>Logout</button>
+    <>
+      <h1 className="text-center text-3xl font-bold my-4">{session.user?.email?.slice(0, 8)}'s Home</h1>
+      <div className="flex justify-center items-center flex-row flex-wrap gap-4 p-4">
+        {devices.map((device, index) => {
+          return <Card key={index} device={device} fetchDevices={fetchDevices}/>
+        })}
+      </div>
+    </>
   )
 }
---
+++ components/card.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+
+interface Device {
+    id: string
+    name: string
+    status: string
+}
+
+
+const Card = ({ device, fetchDevices }: { device: Device, fetchDevices: any }) => {
+  const toggleCard = async () => {
+    var status;
+
+    if (device.status == 'opened') {
+        status = 'closed'
+    } else {
+        status = 'opened'
+    }
+
+    const res = await fetch("/api/door", {
+        method: "PUT",
+        body: JSON.stringify({
+          id: device.id,
+          name: device.name,
+          status: status
+        }),
+        headers: { "Content-Type": "application/json" },
+    });
+
+    if (res.ok) {
+        fetchDevices();
+    }
+  };
+
+  return (
+    <div className="min-w-[20vw] mx-auto rounded-md overflow-hidden shadow-lg bg-zinc text-white">
+      <div className="bg-zinc-700 px-6 py-4 flex items-center justify-center flex-col gap-2 relative">
+        <h2 className="font-bold text-2xl">{device.name}</h2>
+        <motion.div
+          className={`w-6 h-6 rounded-full my-4 ${device.status == 'opened' ? 'bg-green-500' : 'bg-red-500'}`}
+          animate={{ opacity: [0, 1, 0], scale: [1, 1.5, 1] }}
+          transition={{ duration: 2, repeat: Infinity }}
+        />
+        <motion.button
+          className={`inline-block rounded-md px-4 py-2 text-lg font-semibold ${
+            device.status == 'opened' ? 'bg-green-500 text-white' : 'bg-red-500 text-white'
+          }`}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          onClick={toggleCard}
+        >
+          {device.status == 'opened' ? 'Opened' : 'Closed'}
+        </motion.button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
